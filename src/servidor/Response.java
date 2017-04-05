68912b81686dbb258166833cc5ce258d14e76972
douglasmcarmona <douglasmcarmona@gmail.com>
2017-04-05 12:34:19 -0300

    Add files via upload
    so pra garantir

---
+++ src/servidor/Response.java
@@ -0,0 +1,75 @@
+package servidor;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet("/Response")
+public class Response extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Response() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		this.destroy();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		out.println(
+			  "<!DOCTYPE html>\n"
+			+ "<html>"
+			+ 	"<head><title>Hello</title></head>"
+			+ 	"<body>"
+			+ 		"<h1>Hello, " + request.getParameter("nome") + "</h1>"
+			+ 	"</body>"					  
+			+ "</html>"
+		);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		Map<String,String[]> m = request.getParameterMap();
+		Formulario form = new Formulario(m);
+		Simplex.form = form;
+		Simplex.PegarEquacao();
+	}
+
+}
